55acccf410ce2a11d9869c22d3abfc76d1526a14
unknown <admin@how2die.com>
2021-10-21 14:44:28 +0200

    fix indentation

--- model/ImagePost.py
+++ model/ImagePost.py
@@ -2,14 +2,14 @@ import datetime
 
 
 class ImagePost:
-	def __init__(self, id : int, image : str, thumb : str, last_seen : datetime, summary : str):
-		self.id = id
-		self.image = image
-		self.thumb = thumb
-		self.summary = summary
-		self.last_seen = last_seen
+    def __init__(self, id: int, image: str, thumb: str, last_seen: datetime, summary: str):
+        self.id = id
+        self.image = image
+        self.thumb = thumb
+        self.summary = summary
+        self.last_seen = last_seen
 
-	def comments(self):
-                if not 'replies' in self.summary:
-                    return []
-		return [reply['com'] if 'com' in reply else "" for reply in self.summary['replies']]
+    def comments(self):
+        if not 'replies' in self.summary:
+            return []
+        return [reply['com'] if 'com' in reply else "" for reply in self.summary['replies']]
